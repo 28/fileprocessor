46f044c1a2d52d500c17f38c33c85eae95300a11
Dejan Josifovic <Rajoduo@yahoo.com>
2014-07-31 00:07:21 +0200

    Modified type-of-file and aded replace function just for text.

--- src/fileprocessor/raw_file_processor.clj
+++ src/fileprocessor/raw_file_processor.clj
@@ -7,12 +7,9 @@
 (def files-processed (atom 0))
 
 (defn type-of-file
-  "Returns the type of the file as extention."
-  [file]
-  (let [i (.indexOf (.getName file) ".")]
-    (if
-        (> i -1)
-      (subs (.getName file) i))))
+  "Returns the type of the file as extention with dot(.)."
+  [^File file]
+  (subs (.getName file) (.indexOf (.getName file) ".")))
 
 (defn replace-text-with-regex
   ""
@@ -32,4 +29,25 @@
         (with-open [o (writer f :append false)]
           (.write o fsr))
         (swap! files-processed inc)))
+  @files-processed)
+
+(defn replace-text-with-text
+  ""
+  [^String directory-path  ^String file-type  ^String old-text ^String new-text]
+  (reset! files-processed 0)
+  (if
+    (or
+      (nil? directory-path)
+      (not (.isDirectory (file directory-path)))
+      (nil? old-text)
+      (nil? new-text))
+    (throw (IllegalArgumentException. "Illegal directory or text.")))
+  (doseq
+    [f (rest (file-seq (file directory-path)))]
+    (let [fs (slurp f)
+          fsr (replace fs old-text new-text)]
+      (if (not= fs fsr)
+        (with-open [o (writer f :append false)]
+        (.write o fsr)
+          (swap! files-processed inc)))))
   @files-processed)
