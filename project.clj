93a536fc91e987c92193dcc1043b572baf951a4b
Dejan Josifovic <Rajoduo@yahoo.com>
2015-03-02 22:25:10 +0100

    Create a html processing namespace and add dep for enlive.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                 of various files and file types."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.5"]])
---
+++ src/com/paranoidtimes/fileprocessor/html.clj
@@ -0,0 +1,1 @@
+(ns com.paranoidtimes.fileprocessor.html)
