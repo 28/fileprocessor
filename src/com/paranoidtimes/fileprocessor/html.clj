42196bca726b90848d5ff9a3a09f23e7838629c2
Dejan Josifovic <Rajoduo@yahoo.com>
2015-04-07 01:13:34 +0200

    Extract function for enlive nodes; implemented generic assert-select and content version.

--- src/com/paranoidtimes/fileprocessor/html.clj
+++ src/com/paranoidtimes/fileprocessor/html.clj
@@ -2,8 +2,19 @@
   (:require [net.cgrand.enlive-html :as h])
   (:import [java.io.StringReader]))
 
-(defn assertSelect
+(defn- to-enlive-selector
+  ""
+  [selector]
+  (vector (keyword selector)))
+
+(defn assert-select
+  ""
+  [html node function & flags]
+  (let [res (h/html-resource (java.io.StringReader. html))
+        flags (set flags)]
+    (apply function (-> (h/select res (to-enlive-selector node)) first :content))))
+
+(defn assert-select-content
   ""
   [html node content]
-  (let [res (h/html-resource (java.io.StringReader. html))]
-    (= content (-> (h/select res (vector (keyword node))) first :content first))))
+  (assert-select html node #(= % content)))
