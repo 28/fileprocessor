d97353976b352ec3ad263b05fb748b7916a69ca2
Dejan Josifovic <dejan.josifovic@igt.com>
2015-07-01 18:15:54 +0200

    Switched to Clojure 1.7.0.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljfmt "0.1.10"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive "1.1.5"]])
