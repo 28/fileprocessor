3d00b2c68af878ef7b96f9c1586f55bb404dcb89
Dejan Josifovic <dejan.josifovic@igt.com>
2015-07-22 18:12:29 +0200

    Extract test dir to def.

--- test/directory_processor_test.clj
+++ test/directory_processor_test.clj
@@ -2,12 +2,14 @@
   (:require [clojure.test :refer :all]
             [com.paranoidtimes.fileprocessor.directory-processor :refer :all]))
 
+(defonce test-dir "./test/resources")
+
 (deftest directory-processor-testing
   (testing "files-in-directory returns all files in dir"
-    (is (some #(.contains (.getName %) "test.html") (files-in-directory "./test/resources")))
-    (is (some #(.contains (.getName %) "test.txt") (files-in-directory "./test/resources")))
-    (is (= 2 (count (files-in-directory "./test/resources")))))
+    (is (some #(.contains (.getName %) "test.html") (files-in-directory test-dir)))
+    (is (some #(.contains (.getName %) "test.txt") (files-in-directory test-dir)))
+    (is (= 2 (count (files-in-directory test-dir)))))
   (testing "files-in-directory returns empty list when there are no files of the specified file type"
     (is (empty? (files-in-directory "./test/resource" ".clj"))))
   (testing "files-in-directory return a list of files of the specified file type"
-    (is (some #(.contains (.getName %) "test.txt") (files-in-directory "./test/resources" ".txt")))))
+    (is (some #(.contains (.getName %) "test.txt") (files-in-directory test-dir ".txt")))))
