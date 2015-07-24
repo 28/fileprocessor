c37b85991f5cc605a98cb752d87426b362f5759e
Dejan Josifovic <dejan.josifovic@igt.com>
2015-07-24 13:15:26 +0200

    Add test for raw file processor; fix directory test.

--- test/directory_processor_test.clj
+++ test/directory_processor_test.clj
@@ -10,6 +10,6 @@
     (is (some #(.contains (.getName %) "test.txt") (files-in-directory test-dir)))
     (is (= 2 (count (files-in-directory test-dir)))))
   (testing "files-in-directory returns empty list when there are no files of the specified file type"
-    (is (empty? (files-in-directory "./test/resource" ".clj"))))
+    (is (empty? (files-in-directory test-dir ".clj"))))
   (testing "files-in-directory return a list of files of the specified file type"
     (is (some #(.contains (.getName %) "test.txt") (files-in-directory test-dir ".txt")))))
---
+++ test/raw_file_processor_test.clj
@@ -0,0 +1,10 @@
+(ns raw_file_processor_test
+  (:require [clojure.test :refer :all] 
+            [com.paranoidtimes.fileprocessor.raw-file-processor :refer :all]))
+
+(deftest raw-file-processor-test
+  (testing "raw file processor will change contents of the file"
+    (is (= 1 (replace-text-in-files "./test/resources" "LOREM IPSUM" "lorem ipsum" #{".txt"})))
+    (is (= 1 (replace-text-in-files "./test/resources" "lorem ipsum" "LOREM IPSUM" #{".txt"}))))
+  (testing "raw file processor will not attempt to change the contents if old and new patter are the same"
+    (is (= 0 (replace-text-in-files "./test/resources" "LOREM IPSUM" "LOREM IPSUM" #{".txt"})))))
