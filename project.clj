2e4dfa8528ed90f7f7fe8d5022c34cfbf80c9991
Dejan Josifovic <dejan.josifovic@igt.com>
2015-09-07 16:36:05 +0200

    Added dev profile and user namespace; added custom repl prompt.

--- project.clj
+++ project.clj
@@ -5,5 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljfmt "0.1.10"]]
+  :profiles {:dev 
+             {:source-paths ["dev"]}}
+  :repl-options {:prompt (fn [ns] (str "[" ns "]$ "))}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive "1.1.5"]])
