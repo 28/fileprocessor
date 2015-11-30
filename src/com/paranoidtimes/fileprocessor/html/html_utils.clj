f7b76a37b153e44bf56f8b3e5fc29ed4a3109538
Dejan Josifovic <dejan.josifovic@igt.com>
2015-11-30 18:37:05 +0100

    Implement to-enlive-selector to support multiple tags.

--- src/com/paranoidtimes/fileprocessor/html/html_utils.clj
+++ src/com/paranoidtimes/fileprocessor/html/html_utils.clj
@@ -1,16 +1,21 @@
 (ns com.paranoidtimes.fileprocessor.html.html-utils
-  (:require [net.cgrand.enlive-html :as h]))
+  (:require [net.cgrand.enlive-html :as h]
+            [clojure.string :as s]))
 
 (defn to-enlive-selector
-  ""
+  "Transforms the input to a enlive style selector - a vector
+   of tag name keywords."
   [selector]
-  (vector (keyword selector)))
+  (into [] (map keyword (s/split selector #"\s"))))
 
 (defmulti to-res
+  "Converts the given resource to enlive html-resource."
   (fn [obj] (type obj)))
 
 (defmethod to-res java.lang.String [s]
+  "Converts the String resource to enlive html-resource."
   (h/html-resource (java.io.StringReader. s)))
 
-(defmethod to-res java.io.File [f]
-  (h/html-resource f))
+(defmethod to-res :default [r]
+  "Default to-res method."
+  (h/html-resource r))
---
+++ test/html_utils_test.clj
@@ -0,0 +1,11 @@
+(ns html_utils_test
+  (:require [clojure.test :refer :all]
+            [com.paranoidtimes.fileprocessor.html.html-utils :refer :all]))
+
+(deftest test-to-enlive-selector
+  (testing "function-returns-enlive-selector-for-one-tag"
+    (is (= [:div]
+           (to-enlive-selector "div"))))
+  (testing "function-returns-enlive-selector-for-multiple-tags"
+    (is (= [:div :div :p]
+           (to-enlive-selector "div div p")))))
